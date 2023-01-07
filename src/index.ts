8d5120847aaa1da0b6eaffed63578c8cb7e55efc
charan678 <charan678@hotmail.com>
2023-01-08 01:13:17 +0530

    chore: setup express project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Cloud Document Text search")
+import express, { Request, Response } from "express";
+const app = express();
+
+app.use(express.json());
+
+app.use(
+
+);
+
+app.listen(8000, async () => {
+  
+});
